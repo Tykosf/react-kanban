0dc4d6499523bbf6c48ceb13577f08fe634549f6
Tykosf <prromanyi@gmail.com>
2019-10-26 12:30:30 +0300

    deploy

---
+++ build/precache-manifest.bb9d99d28969260a029ed7c4250677b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8b84a2468344aba795e5130181eaf7",
+    "url": "/react-kanban/index.html"
+  },
+  {
+    "revision": "2831b10ed3985636594a",
+    "url": "/react-kanban/static/css/main.0157a8f5.chunk.css"
+  },
+  {
+    "revision": "cd094a748b4cca1917d5",
+    "url": "/react-kanban/static/js/2.27e8e6e1.chunk.js"
+  },
+  {
+    "revision": "2831b10ed3985636594a",
+    "url": "/react-kanban/static/js/main.bd45efe2.chunk.js"
+  },
+  {
+    "revision": "d0d3ccd6e858342af903",
+    "url": "/react-kanban/static/js/runtime-main.3a7034f3.js"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import cards from './Cards';
-
-export default combineReducers({
-	cards
-});
